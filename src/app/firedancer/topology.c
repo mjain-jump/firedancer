0e1de903464d2bef130d75f0e10ee00479de6946
Manik Jain <mjain@jumptrading.com>
2026-08-28 00:35:53 +0000

    restore: delete snapwr tile

--- src/app/firedancer/topology.c
+++ src/app/firedancer/topology.c
@@ -1546,9 +1546,6 @@ fd_topo_configure_tile( fd_topo_tile_t * tile,
     tile->snapin.snoop_obj_id = fd_pod_query_ulong( config->topo.props, "snapio_snoop", ULONG_MAX );
     tile->snapin.alpenglow = config->firedancer.development.alpenglow;
 
-  } else if( FD_UNLIKELY( !strcmp( tile->name, "snapwr" ) ) ) {
-    tile->snapwr.partition_sz = config->development.accdb.partition_size_gib*(1UL<<30UL);
-
   } else if( FD_UNLIKELY( !strcmp( tile->name, "repair" ) ) ) {
     tile->repair.max_pending_shred_sets    = config->tiles.shred.max_pending_shred_sets;
     tile->repair.max_shreds_per_block      = config->limits.max_shreds_per_block;
